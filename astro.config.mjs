6acd4d1a055cda486ca1ef95512208fd02045966
Ghoes18 <gohxn123@gmail.com>
2026-03-06 17:23:04 +0000

    fix: setup astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://goncaloguimaraes.com",
-  // base: "/portfolio/", // descomente se a URL for ghoes18.github.io/portfolio/
+  site: "https://ghoes18.github.io",
+  base: "/portfolio/",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
